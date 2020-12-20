334634112041012c90cdd9c91af1b2346abd8985
Pavle Ilic <pavle.ilic24d@gmail.com>
2020-12-20 23:25:20 +0100

    finish html,css,javascript editors

---
+++ main.js
@@ -0,0 +1,17 @@
+var myCodeMirror = CodeMirror(document.getElementById('html__editor'), {
+    lineNumbers: true,
+    mode: "html",
+    theme: "ayu-mirage"
+});
+
+var myCodeMirror = CodeMirror(document.getElementById('js__editor'), {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: "ayu-mirage"
+});
+
+var myCodeMirror = CodeMirror(document.getElementById('css__editor'), {
+    lineNumbers: true,
+    mode: "css",
+    theme: "ayu-mirage"
+});
